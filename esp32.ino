e4036a984a7a081e0db34e343bd9882e553304e4
hathai25 <phamhuyhathai@gmail.com>
2024-06-07 17:44:42 +0700

    REFACTOR: optimize & add comment

--- esp32.ino
+++ esp32.ino
@@ -3,32 +3,32 @@
 #include <U8x8lib.h>
 #include <Preferences.h>
 
+// Define for OLED screen
 #define OLED_SCK_PIN 22
 #define OLED_SDA_PIN 21
-
-Preferences preferences;
-
-U8X8_SH1106_128X64_NONAME_HW_I2C u8x8(/* reset=*/U8X8_PIN_NONE, OLED_SCK_PIN, OLED_SDA_PIN);
-
 #define U8LOG_WIDTH 16
 #define U8LOG_HEIGHT 8
-uint8_t u8log_buffer[U8LOG_WIDTH * U8LOG_HEIGHT];
-U8X8LOG u8x8log;
 
+U8X8_SH1106_128X64_NONAME_HW_I2C u8x8(/* reset=*/U8X8_PIN_NONE, OLED_SCK_PIN, OLED_SDA_PIN);
+uint8_t u8log_buffer[U8LOG_WIDTH * U8LOG_HEIGHT];
+U8X8LOG u8x8log; //Instance of OLED screen
+
+//Define for RFID RC522
 #define SS_PIN 5
 #define RST_PIN 2
-#define PIN_BUZZER 4
-
-const int ledPin = 13; // GPIO pin connected to the LED
-const int LED1 = 26;
-
-MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
+MFRC522 rfid(SS_PIN, RST_PIN); // Instance of rfid reader
 MFRC522::MIFARE_Key key;
 
-// Init array that will store new NUID
-byte nuidPICC[4];
-byte preNuidPICC[4];
-byte pre2NuidPICC[4];
+//Define other GPIO component
+#define BUZZER_PIN 4
+#define LED_PIN 13
+#define SETUP_LED 26
+Preferences preferences;
+
+
+byte currentNuidPICC[4];
+byte prevNuidPICC[4];
+byte prevPrevNuidPICC[4];
 
 /**
  * Helper routine to dump a byte array as hex values to Serial.
@@ -65,30 +65,19 @@ String toString(byte *buffer, byte bufferSize)
   return result;
 }
 
-/**
- * Helper routine to dump a byte array as hex values to Serial.
- */
-void dump_byte_array(byte *buffer, byte bufferSize)
-{
-  for (byte i = 0; i < bufferSize; i++)
-  {
-    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-    Serial.print(buffer[i], HEX);
-  }
-}
-
 void setup()
 {
-
-  pinMode(LED1, OUTPUT);
+  pinMode(SETUP_LED, OUTPUT);
+  Serial.begin(115200);
+  //setup oled
   u8x8.begin();
   u8x8.setFont(u8x8_font_chroma48medium8_r);
   u8x8log.begin(u8x8, U8LOG_WIDTH, U8LOG_HEIGHT, u8log_buffer);
   u8x8log.setRedrawMode(1);
 
-  Serial.begin(115200);
-  pinMode(ledPin, OUTPUT); // Set the LED pin as an output
-  pinMode(PIN_BUZZER, OUTPUT);
+  //Setup gpios
+  pinMode(LED_PIN, OUTPUT); // Set the LED pin as an output
+  pinMode(BUZZER_PIN, OUTPUT);
   SPI.begin();     // Init SPI bus
   rfid.PCD_Init(); // Init MFRC522
 
@@ -97,24 +86,25 @@ void setup()
     key.keyByte[i] = 0xFF;
   }
 
-  preferences.begin("my-app", false);
+  preferences.begin("rfid-reader", false);
 
-  preferences.getBytes("prev2", pre2NuidPICC, 4);
-  preferences.getBytes("prev", preNuidPICC, 4);
-  preferences.getBytes("now", nuidPICC, 4);
+  preferences.getBytes("prevPrev", prevPrevNuidPICC, 4);
+  preferences.getBytes("prev", prevNuidPICC, 4);
+  preferences.getBytes("current", currentNuidPICC, 4);
 
-  String now = toString(nuidPICC, 4);
-  String prev = toString(preNuidPICC, 4);
-  String prev2 = toString(pre2NuidPICC, 4);
+  String currentNuidString = toString(currentNuidPICC, 4);
+  String prevNuidString = toString(prevNuidPICC, 4);
+  String prevPrevNuidString = toString(prevPrevNuidPICC, 4);
 
-  u8x8log.print("Prev 3: ");
-  u8x8log.print(prev2);
+  u8x8log.println("Three last Nuid:");
+  u8x8log.print("#1: ");
+  u8x8log.print(currentNuidString);
   u8x8log.print("\n");
-  u8x8log.print("Prev 2: ");
-  u8x8log.print(prev);
+  u8x8log.print("#2: ");
+  u8x8log.print(prevNuidString);
   u8x8log.print("\n");
-  u8x8log.print("Prev 1: ");
-  u8x8log.print(now);
+  u8x8log.print("#3: ");
+  u8x8log.print(prevPrevNuidString);
   u8x8log.print("\n");
 
   Serial.println(F("This code scan the MIFARE Classsic NUID."));
@@ -131,8 +121,10 @@ void loop()
 
   // Verify if the NUID has been readed
   if (!rfid.PICC_ReadCardSerial())
+    u8x8log.println("Card read previously");
     return;
 
+  //Get card type
   Serial.print(F("PICC type: "));
   MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
   Serial.println(rfid.PICC_GetTypeName(piccType));
@@ -144,48 +136,55 @@ void loop()
     return;
   }
 
-  if (rfid.uid.uidByte[0] != nuidPICC[0] || rfid.uid.uidByte[1] != nuidPICC[1] || rfid.uid.uidByte[2] != nuidPICC[2] || rfid.uid.uidByte[3] != nuidPICC[3])
+  if (rfid.uid.uidByte[0] != currentNuidPICC[0] || rfid.uid.uidByte[1] != currentNuidPICC[1] || rfid.uid.uidByte[2] != currentNuidPICC[2] || rfid.uid.uidByte[3] != currentNuidPICC[3])
   {
     Serial.println(F("A new card has been detected."));
 
-    // Store NUID into nuidPICC array
+    // Store NUID into currentNuidPICC, prevNuidPICC, prevPrevNuidPICC array
     for (byte i = 0; i < 4; i++)
     {
-      pre2NuidPICC[i] = preNuidPICC[i];
-      preNuidPICC[i] = nuidPICC[i];
-      nuidPICC[i] = rfid.uid.uidByte[i];
+      prevPrevNuidPICC[i] = prevNuidPICC[i];
+      prevNuidPICC[i] = currentNuidPICC[i];
+      currentNuidPICC[i] = rfid.uid.uidByte[i];
     }
+
+    //Loggin purpose
     Serial.println(F("The NUID tag is:"));
     Serial.print(F("In hex: "));
     printHex(rfid.uid.uidByte, rfid.uid.size);
     Serial.println();
     Serial.print(F("In dec: "));
-    digitalWrite(ledPin, 1);
-    digitalWrite(PIN_BUZZER, HIGH);
-    printDec(rfid.uid.uidByte, rfid.uid.size);
+
+    //Active LED, BUZZER
+    digitalWrite(LED_PIN, 1);
+    digitalWrite(BUZZER_PIN, HIGH);
     // Print to screen
-    String now = toString(rfid.uid.uidByte, rfid.uid.size);
-    String prev = toString(preNuidPICC, 4);
-    String prev2 = toString(pre2NuidPICC, 4);
+    String current = toString(rfid.uid.uidByte, rfid.uid.size);
+    String prev = toString(prevNuidPICC, 4);
+    String prevPrev = toString(prevPrevNuidPICC, 4);
 
-    preferences.putBytes("prev2", pre2NuidPICC, 4);
-    preferences.putBytes("prev", preNuidPICC, 4);
-    preferences.putBytes("now", rfid.uid.uidByte, 4);
+    preferences.putBytes("prevPrev", prevPrevNuidPICC, 4);
+    preferences.putBytes("prev", prevNuidPICC, 4);
+    preferences.putBytes("current", rfid.uid.uidByte, 4);
 
-    u8x8log.print("Prev 2: ");
-    u8x8log.print(prev2);
+    u8x8log.println("---------");
+    u8x8log.println("Current NUID: ");
+    u8x8log.print(current);
     u8x8log.print("\n");
-    u8x8log.print("Prev 1: ");
+    u8x8log.println("Prev NUID: ");
     u8x8log.print(prev);
     u8x8log.print("\n");
-    u8x8log.print("Now: ");
-    u8x8log.print(now);
+    u8x8log.println("Pre-prev NUID: ");
+    u8x8log.print(prevPrev);
     u8x8log.print("\n");
     delay(100);
-    digitalWrite(ledPin, 0);
-    digitalWrite(PIN_BUZZER, LOW);
+
+    //Turn off LED & BUZZER
+    digitalWrite(LED_PIN, 0);
+    digitalWrite(BUZZER_PIN, LOW);
   }
   else
+    u8x8log.println("Card read previously");
     Serial.println(F("Card read previously."));
 
   // Halt PICC
